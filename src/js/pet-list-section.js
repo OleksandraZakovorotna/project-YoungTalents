6fa905a173d7f75137e2b606318e02dda8bcefdc
asia <asia.skoromna@gmail.com>
2026-03-06 19:08:41 +0300

    update pet section styles and learn-more js logic

--- src/js/pet-list-section.js
+++ src/js/pet-list-section.js
@@ -1,18 +1,22 @@
+import { openPetModal } from './animal-details-modal.js';
 
-document.addEventListener('click', e => {
-  const btn = e.target.closest('.learn-more-btn');
-  if (!btn) return;
+export function initLearnMoreButtons() {
+  document.addEventListener('click', e => {
+    const btn = e.target.closest('.learn-more-btn');
+    if (!btn) return;
 
-  const pet = {
-    name: btn.dataset.name,
-    species: btn.dataset.species,
-    image: btn.dataset.img,
-    description: btn.dataset.desc,
-    age: btn.dataset.age,
-    sex: btn.dataset.sex
-  };
+    const pet = {
+      name: btn.dataset.name,
+      species: btn.dataset.species,
+      image: btn.dataset.img,
+      description: btn.dataset.desc,
+      age: btn.dataset.age,
+      gender: btn.dataset.sex
+    };
 
-  openPetModal(pet);
-});
+    openPetModal(pet);
+  });
+}
+
 
 
