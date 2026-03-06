da934e5016bb106641a545372cbf231fb7e5d1a7
OleksandraZakovorotna <x.boogieboo.x@gmail.com>
2026-03-06 08:22:29 +0000

    Deploying to gh-pages from @ OleksandraZakovorotna/project-YoungTalents@eddf8163d723f6a9fc3f4e7ce9376ee68db158de 🚀

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
-(function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))i(e);new MutationObserver(e=>{for(const r of e)if(r.type==="childList")for(const o of r.addedNodes)o.tagName==="LINK"&&o.rel==="modulepreload"&&i(o)}).observe(document,{childList:!0,subtree:!0});function s(e){const r={};return e.integrity&&(r.integrity=e.integrity),e.referrerPolicy&&(r.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?r.credentials="include":e.crossOrigin==="anonymous"?r.credentials="omit":r.credentials="same-origin",r}function i(e){if(e.ep)return;e.ep=!0;const r=s(e);fetch(e.href,r)}})();
+import{S as a,N as l,P as d}from"./assets/vendor-Dn2PEXfL.js";(function(){const s=document.createElement("link").relList;if(s&&s.supports&&s.supports("modulepreload"))return;for(const t of document.querySelectorAll('link[rel="modulepreload"]'))n(t);new MutationObserver(t=>{for(const e of t)if(e.type==="childList")for(const o of e.addedNodes)o.tagName==="LINK"&&o.rel==="modulepreload"&&n(o)}).observe(document,{childList:!0,subtree:!0});function r(t){const e={};return t.integrity&&(e.integrity=t.integrity),t.referrerPolicy&&(e.referrerPolicy=t.referrerPolicy),t.crossOrigin==="use-credentials"?e.credentials="include":t.crossOrigin==="anonymous"?e.credentials="omit":e.credentials="same-origin",e}function n(t){if(t.ep)return;t.ep=!0;const e=r(t);fetch(t.href,e)}})();const c="/icons.svg",u="https://paw-hut.b.goit.study/api/feedbacks?limit=6&page=1";function f(i){const s=i.rate;let r="";for(let n=1;n<=5;n++)n<=s?r+=`
+        <svg class="star-icon filled">
+          <use href="${c}#icon-star-full"></use>
+        </svg>`:n-.5<=s?r+=`
+        <svg class="star-icon">
+          <use href="${c}#icon-star-half"></use>
+        </svg>`:r+=`
+        <svg class="star-icon outline">
+          <use href="${c}#icon-star-empty"></use>
+        </svg>`;return`
+    <div class="swiper-slide">
+      <div class="feedback-card">
+        <div class="rating-container">
+          <div class="stars-wrapper">${r}</div>
+        </div>
+
+        <p class="feedback-text">${i.description}</p>
+        <p class="feedback-author">${i.author}</p>
+      </div>
+    </div>
+  `}function p(i){const s=document.getElementById("feedbacks-wrapper");!s||!(i!=null&&i.length)||(s.innerHTML=i.map(f).join(""),g())}function g(){const i=document.querySelector(".feedbacks-section .feedbacks-slider");if(!i)return;const s=i.querySelector(".swiper-button-next"),r=i.querySelector(".swiper-button-prev"),n=new a(i,{modules:[l,d],slidesPerView:1,slidesPerGroup:1,spaceBetween:32,grabCursor:!0,pagination:{el:i.querySelector(".swiper-pagination"),clickable:!0},breakpoints:{320:{slidesPerView:1,slidesPerGroup:1},768:{slidesPerView:2,slidesPerGroup:1},1280:{slidesPerView:2,slidesPerGroup:1}},on:{init:function(){t(this)},slideChange:function(){t(this)}}});function t(e){!s||!r||(r.disabled=e.isBeginning,r.classList.toggle("swiper-button-disabled",e.isBeginning),s.disabled=e.isEnd,s.classList.toggle("swiper-button-disabled",e.isEnd))}return s&&s.addEventListener("click",e=>{e.preventDefault(),e.stopPropagation(),n.slideNext()}),r&&r.addEventListener("click",e=>{e.preventDefault(),e.stopPropagation(),n.slidePrev()}),n}async function b(){if(document.querySelector(".feedbacks-section"))try{const n=(await(await fetch(u)).json()).feedbacks;p(n)}catch{const r=document.getElementById("feedbacks-error");r&&r.classList.remove("is-hidden")}}b();
 //# sourceMappingURL=index.js.map
